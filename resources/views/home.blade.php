c9697795ab14b160176d3ac74ae8cb1b41d29317
codarella <sadjeroh@gmail.com>
2025-01-08 19:32:35 +0100

    changed the components 'c' to small letters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Hello world
-</body>
-</html>
+<x-layout>
+    <x-slot:heading>
+        Home page
+    </x-slot:heading>
+
+   
+
+        
+
+
+
+
+    <h1>Welcome to the home page</h1>
+</x-layout>
